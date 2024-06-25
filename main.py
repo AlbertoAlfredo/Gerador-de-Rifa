58488f859b0dd9266b82fbf4a0c49ca06d87fe81
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2024-06-25 14:58:14 -0300

    atualizado para geração com python
    Sistema atualizado apra receber um txt com nomes separados por quebra de linha no arquivo nomes.txt, com python 3+ instalado execute o main.py e ele irá gerar o html com o diploma em branco, o Proerd nos manda o diploma e nós colocamos os nomes

---
+++ main.py
@@ -0,0 +1,29 @@
+import codecs
+
+nomes = open("nomes.txt", "r")
+nomes_alunos = nomes.read().splitlines()
+
+with codecs.open("base/head.html", 'rb', 'utf-8') as header:
+    head = header.read()
+with codecs.open("base/footer.html", 'rb', 'utf-8') as footer:
+    foot = footer.read()
+
+
+diploma = ""
+
+
+
+for nome in nomes_alunos:
+    diploma += """
+        <section class="diploma">
+        <div class="nome">
+            <p>""" + nome + """</p>
+        </div>
+        </section>
+        """
+
+diploma = head + diploma + foot
+
+with open("diploma.html", "w") as diplomas_impressao:
+    diplomas_impressao.write(diploma)
+
