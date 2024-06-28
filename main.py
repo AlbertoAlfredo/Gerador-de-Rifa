56066fc1f9b1f47526ddc7dfff58acad83f34b99
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2024-06-27 22:55:13 -0300

    Criado o modulo de gerar o pdf, criado o executável para uso sem instalar o python

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
-import codecs
+from modules.gerar_html import gerar_html
+from modules.gerar_pdf import gerar_pdf
 
-nomes = open("nomes.txt", "r")
-nomes_alunos = nomes.read().splitlines()
-
-with codecs.open("base/head.html", 'rb', 'utf-8') as header:
-    head = header.read()
-with codecs.open("base/footer.html", 'rb', 'utf-8') as footer:
-    foot = footer.read()
-
-
-diploma = ""
-
-
-
-for nome in nomes_alunos:
-    diploma += """
-        <section class="diploma">
-        <div class="nome">
-            <p>""" + nome + """</p>
-        </div>
-        </section>
-        """
-
-diploma = head + diploma + foot
-
-with open("diploma.html", "w") as diplomas_impressao:
-    diplomas_impressao.write(diploma)
-
+gerar_html()
+gerar_pdf()
--- public/pyscript/core.js
+++
@@ -1,2 +0,0 @@
-export{g as MPWorker,f as PyWorker,T as TYPES,h as config,e as hooks,i as inputFailure,o as offline_interpreter,b as optional,s as stdlib,j as whenDefined}from"./core-CVoMgJtU.js";
-//# sourceMappingURL=core.js.map
